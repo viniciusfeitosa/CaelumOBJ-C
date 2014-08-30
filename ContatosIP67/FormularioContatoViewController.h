0a13465ed26c00ad3c5dd4ac7ae52b9ff4b3e962
ios4548 <ios4548@Mac-mini-de-Caelum-8.local>
2014-08-30 10:53:56 -0300

    cap 4

---
+++ ContatosIP67/FormularioContatoViewController.h
@@ -0,0 +1,13 @@
+//
+//  FormularioContatoViewController.h
+//  ContatosIP67
+//
+//  Created by ios4548 on 30/08/14.
+//  Copyright (c) 2014 ios4548. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FormularioContatoViewController : UIViewController
+
+@end
---
+++ ContatosIP67/FormularioContatoViewController.m
@@ -0,0 +1,38 @@
+//
+//  FormularioContatoViewController.m
+//  ContatosIP67
+//
+//  Created by ios4548 on 30/08/14.
+//  Copyright (c) 2014 ios4548. All rights reserved.
+//
+
+#import "FormularioContatoViewController.h"
+
+@interface FormularioContatoViewController ()
+
+@end
+
+@implementation FormularioContatoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
