df714759055b3f1beac9d2e3bfadc500d4bd6854
ios4548 <ios4548@Mac-mini-de-Caelum-8.local>
2014-09-13 15:46:39 -0300

    cap10

--- ContatosIP67/ListaContatosViewController.m
+++ ContatosIP67/ListaContatosViewController.m
@@ -20,6 +20,7 @@
         UIBarButtonItem *botaoExibirFormulario = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(exibeFormulario)];
         
         self.navigationItem.rightBarButtonItem = botaoExibirFormulario;
+        self.navigationItem.leftBarButtonItem = self.editButtonItem;
     }
     return self;
 }
@@ -58,6 +59,13 @@
     Contato *contato = [self.contatos objectAtIndex:indexPath.row];
     cell.textLabel.text = contato.nome;
     return cell;
+}
+
+- (void) tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [self.contatos removeObjectAtIndex:indexPath.row];
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }
 }
 
 @end
